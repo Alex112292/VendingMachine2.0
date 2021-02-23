528e0ff76eeb2c234a22c7690452d65a581ff835
Alex112292 <57815789+Alex112292@users.noreply.github.com>
2021-02-23 13:28:21 -0500

    Prints out selections and has user enter money

--- src/BetterVendingMachine.java
+++ src/BetterVendingMachine.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class BetterVendingMachine {
     public static void main(String[] args)
     {
-        
+        System.out.println("Welcome to the Non-Trademarked Vending Machine! It's the vending machine that sells the opposites of the popular brands!");
+        System.out.println("This machine only accepts $1 bills and will return exact change from your selection.");
+        System.out.println("[A1] Macadamia Sad - $1.00, [B1] Stand's Chips - $2.00, [C1] Road Mix - $1.00, [A2] Andromeda - $3.00, [B2] Venus Bar - $1.00. [C2]  - $2.00");
+        System.out.println("How much money would you like to insert?");
+
+        Scanner input = new Scanner(System.in);
+        Double balance = input.nextDouble();
+        int balanceInt = (int)Math.round(balance);
+
+        System.out.println("Your current balance is: $" + balanceInt + ".");
     }
 }
